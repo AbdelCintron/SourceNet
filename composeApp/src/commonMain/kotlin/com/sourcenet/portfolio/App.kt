76473daed94868b7e5c1265308500d92051b75ad
abdel <abdelcintron@gmail.com>
2025-12-03 23:19:17 -0500

    Added LICENSE and Hello World

--- composeApp/src/commonMain/kotlin/com/sourcenet/portfolio/App.kt
+++ composeApp/src/commonMain/kotlin/com/sourcenet/portfolio/App.kt
@@ -32,7 +32,7 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+                Text("Hello World")
             }
             AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
